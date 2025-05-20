618e556053384a48034d4986546f7b8cd8d66011
Jonas Scholz <jonas.scholz123@gmail.com>
2025-05-20 19:27:49 +0100

    Make model completely configurable

--- src/cdnp/model/ddpm.py
+++ src/cdnp/model/ddpm.py
@@ -1,122 +1,67 @@
-import numpy as np
 import torch
-import torch.nn as nn
-
-
-def ddpm_schedules(beta1, beta2, T):
-    """
-    Returns pre-computed schedules for DDPM sampling, training process.
-    """
-    assert beta1 < beta2 < 1.0, "beta1 and beta2 must be in (0, 1)"
-
-    beta_t = (beta2 - beta1) * torch.arange(0, T + 1, dtype=torch.float32) / T + beta1
-    sqrt_beta_t = torch.sqrt(beta_t)
-    alpha_t = 1 - beta_t
-    log_alpha_t = torch.log(alpha_t)
-    alphabar_t = torch.cumsum(log_alpha_t, dim=0).exp()
-
-    sqrtab = torch.sqrt(alphabar_t)
-    oneover_sqrta = 1 / torch.sqrt(alpha_t)
-
-    sqrtmab = torch.sqrt(1 - alphabar_t)
-    mab_over_sqrtmab_inv = (1 - alpha_t) / sqrtmab
-
-    return {
-        "alpha_t": alpha_t,  # \alpha_t
-        "oneover_sqrta": oneover_sqrta,  # 1/\sqrt{\alpha_t}
-        "sqrt_beta_t": sqrt_beta_t,  # \sqrt{\beta_t}
-        "alphabar_t": alphabar_t,  # \bar{\alpha_t}
-        "sqrtab": sqrtab,  # \sqrt{\bar{\alpha_t}}
-        "sqrtmab": sqrtmab,  # \sqrt{1-\bar{\alpha_t}}
-        "mab_over_sqrtmab": mab_over_sqrtmab_inv,  # (1-\alpha_t)/\sqrt{1-\bar{\alpha_t}}
-    }
+from diffusers import DDPMScheduler
+from torch import nn
 
 
 class DDPM(nn.Module):
-    def __init__(self, nn_model, betas, n_T, device, drop_prob=0.1):
+    def __init__(
+        self,
+        model: nn.Module,
+        loss_fn: nn.Module,
+        noise_scheduler: DDPMScheduler,
+        device: str,
+    ):
         super(DDPM, self).__init__()
-        self.nn_model = nn_model.to(device)
-
-        # register_buffer allows accessing dictionary produced by ddpm_schedules
-        # e.g. can access self.sqrtab later
-        for k, v in ddpm_schedules(betas[0], betas[1], n_T).items():
-            self.register_buffer(k, v)
-
-        self.n_T = n_T
+        self.model = model
+        self.loss_fn = loss_fn
+        self.noise_scheduler = noise_scheduler
+        # TODO: This should support all kinds of context data.
+        # self.context_embedding = context_embedding
         self.device = device
-        self.drop_prob = drop_prob
-        self.loss_mse = nn.MSELoss()
+        self.num_timesteps = noise_scheduler.config.num_train_timesteps
 
-    def forward(self, x, c):
+    def forward(self, x: torch.Tensor, ctx: torch.Tensor) -> torch.Tensor:
         """
-        this method is used in training, so samples t and noise randomly
+        Computes the noise prediction loss for a batch of data.
+
+        :param x: The input data (e.g., images).
+        :param ctx: The context labels (e.g., class labels).
         """
-
-        _ts = torch.randint(1, self.n_T + 1, (x.shape[0],)).to(
-            self.device
-        )  # t ~ Uniform(0, n_T)
-        noise = torch.randn_like(x)  # eps ~ N(0, 1)
-
-        x_t = (
-            self.sqrtab[_ts, None, None, None] * x
-            + self.sqrtmab[_ts, None, None, None] * noise
-        )  # This is the x_t, which is sqrt(alphabar) x_0 + sqrt(1-alphabar) * eps
-        # We should predict the "error term" from this x_t. Loss is what we return.
-
-        # dropout context with some probability
-        context_mask = torch.bernoulli(torch.zeros_like(c) + self.drop_prob).to(
-            self.device
+        noise = torch.randn_like(x)
+        shape = (x.shape[0],)
+        timesteps = (
+            torch.randint(0, self.num_timesteps - 1, shape).long().to(self.device)
         )
+        noisy_x = self.noise_scheduler.add_noise(x, noise, timesteps)
 
-        # return MSE between added noise, and our predicted noise
-        pred_noise = self.nn_model(x_t, c, _ts / self.n_T, context_mask)
-        return self.loss_mse(noise, pred_noise)
+        pred = self.model(noisy_x, timesteps, class_labels=ctx).sample
 
-    def sample(self, n_sample, size, guide_w=0.0):
-        # we follow the guidance sampling scheme described in 'Classifier-Free Diffusion Guidance'
-        # to make the fwd passes efficient, we concat two versions of the dataset,
-        # one with context_mask=0 and the other context_mask=1
-        # we then mix the outputs with the guidance scale, w
-        # where w>0 means more guidance
-        x_i = torch.randn(n_sample, *size).to(
-            self.device
-        )  # x_T ~ N(0, 1), sample initial noise
-        c_i = torch.arange(0, 10).to(
-            self.device
-        )  # context for us just cycles throught the mnist labels
-        c_i = c_i.repeat(int(n_sample / c_i.shape[0]))
+        return self.loss_fn(pred, noise)
 
-        # don't drop context at test time
-        context_mask = torch.zeros_like(c_i).to(self.device)
+    @torch.no_grad()
+    def sample(
+        self,
+        size: tuple,
+        ctx: torch.Tensor = None,
+    ) -> torch.Tensor:
+        """
+        Generates samples from the model.
 
-        # double the batch
-        c_i = c_i.repeat(2)
-        context_mask = context_mask.repeat(2)
-        context_mask[n_sample:] = 1.0  # makes second half of batch context free
+        :param size: Size of the generated samples, should have shape
+            (num_samples, channels, height, width).
+        :param ctx: Context labels for the generation process.
+        :return: Generated samples of shape `size`.
+        """
 
-        x_i_store = []  # keep track of generated steps in case want to plot something
-        for i in range(self.n_T, 0, -1):
-            t_is = torch.tensor([i / self.n_T]).to(self.device)
-            t_is = t_is.repeat(n_sample, 1, 1, 1)
+        x = torch.randn(*size).to(self.device)
 
-            # double batch
-            x_i = x_i.repeat(2, 1, 1, 1)
-            t_is = t_is.repeat(2, 1, 1, 1)
+        for i, t in enumerate(self.noise_scheduler.timesteps):
+            # Get model pred
+            with torch.no_grad():
+                residual = self.model(
+                    x, t, ctx
+                ).sample  # Again, note that we pass in our labels y
 
-            z = torch.randn(n_sample, *size).to(self.device) if i > 1 else 0
-
-            # split predictions and compute weighting
-            eps = self.nn_model(x_i, c_i, t_is, context_mask)
-            eps1 = eps[:n_sample]
-            eps2 = eps[n_sample:]
-            eps = (1 + guide_w) * eps1 - guide_w * eps2
-            x_i = x_i[:n_sample]
-            x_i = (
-                self.oneover_sqrta[i] * (x_i - eps * self.mab_over_sqrtmab[i])
-                + self.sqrt_beta_t[i] * z
-            )
-            if i % 20 == 0 or i == self.n_T or i < 8:
-                x_i_store.append(x_i.detach().cpu().numpy())
-
-        x_i_store = np.array(x_i_store)
-        return x_i, x_i_store
+            # Update sample with step
+            x = self.noise_scheduler.step(residual, t, x).prev_sample
+        return x
--- src/cdnp/plot/plotter.py
+++ src/cdnp/plot/plotter.py
@@ -1,10 +1,8 @@
 # %%
-from typing import Optional, Sequence
+from typing import Optional
 
-import matplotlib.pyplot as plt
 import torch
 from loguru import logger
-from matplotlib.animation import FuncAnimation, PillowWriter
 from matplotlib.figure import Figure
 from mlbnb.paths import ExperimentPath
 from torch.utils.data import Dataset
@@ -18,74 +16,36 @@ class Plotter:
         self,
         device: str | torch.device,
         test_data: Dataset,
-        ws_test: Sequence[float],
         num_samples: int,
         num_classes: int,
+        num_channels: int,
+        sidelength: int,
         save_to: Optional[ExperimentPath] = None,
     ):
         self._device = device
         self._test_data = test_data
         self._dir = save_to
-        self._ws_test = ws_test
         self._num_samples = num_samples
         self._num_classes = num_classes
+        self._num_channels = num_channels
+        self._sidelength = sidelength
 
     @torch.no_grad()
     def plot_prediction(self, model: DDPM, epoch: int = 0) -> Optional[Figure]:
         logger.info("Making and saving prediction plots")
-        num_samples = self._num_samples * self._num_classes
-        for w_i, w in enumerate(self._ws_test):
-            x_gen, x_gen_store = model.sample(num_samples, (1, 28, 28), guide_w=w)
-
-            x_all = torch.cat([x_gen])
-            grid = make_grid(x_all * -1 + 1, nrow=10)
-            path = self._dir.at(f"image_ep{epoch}_w{w}.png")
-            save_image(grid, path)
-
-            if epoch % 5 == 0:
-                self._save_gif(epoch, w, x_gen_store)
-
-    def _save_gif(self, epoch: int, w: float, x_gen_store) -> None:
-        # create gif of images evolving over time, based on x_gen_store
-        fig, axs = plt.subplots(
-            nrows=int(self._num_samples),
-            ncols=self._num_classes,
-            sharex=True,
-            sharey=True,
-            figsize=(8, 3),
+        class_labels = (
+            torch.arange(self._num_classes)
+            .repeat(self._num_samples, 1)
+            .flatten()
+            .to(self._device)
         )
 
-        def animate_diff(i, x_gen_store):
-            plots = []
-            for row in range(int(self._num_samples)):
-                for col in range(self._num_classes):
-                    axs[row, col].clear()
-                    axs[row, col].set_xticks([])
-                    axs[row, col].set_yticks([])
-                    # plots.append(axs[row, col].imshow(x_gen_store[i,(row*n_classes)+col,0],cmap='gray'))
-                    plots.append(
-                        axs[row, col].imshow(
-                            -x_gen_store[i, (row * self._num_classes) + col, 0],
-                            cmap="gray",
-                            vmin=(-x_gen_store[i]).min(),
-                            vmax=(-x_gen_store[i]).max(),
-                        )
-                    )
-            return plots
-
-        ani = FuncAnimation(
-            fig,
-            animate_diff,
-            fargs=[x_gen_store],
-            interval=200,
-            blit=False,
-            repeat=True,
-            frames=x_gen_store.shape[0],
+        total_samples = self._num_samples * self._num_classes
+        x_gen = model.sample(
+            (total_samples, self._num_channels, self._sidelength, self._sidelength),
+            class_labels,
         )
 
-        path = self._dir.at(f"gif_ep{epoch}_w{w}.gif")
-        ani.save(
-            path,
-            dpi=100,
-            writer=PillowWriter(fps=5),
-        )
+        grid = make_grid(x_gen * -1 + 1, nrow=self._num_classes)
+        path = self._dir.at(f"image_ep{epoch}.png")
+        save_image(grid, path)
