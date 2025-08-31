35cf56f6d6cdded20e318abd6adf75e5eaddfefd
Jonas Scholz <jonas.scholz123@gmail.com>
2025-08-31 20:42:24 +0100

    Implement ERA5 wind inpainting

--- src/cdnp/task.py
+++ src/cdnp/task.py
@@ -70,3 +70,43 @@ def preprocess_weather_forecast(
     ctx = torch.cat([static, dyn], dim=1)  # (B, static+dyn, lat, lon)
 
     return ModelCtx(image_ctx=ctx), trg
+
+
+def preprocess_weather_inpaint(
+    batch: tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor],
+    gen: torch.Generator,
+    min_frac: float,
+    max_frac: float,
+) -> tuple[ModelCtx, torch.Tensor]:
+    zero_time, static, dyn, trg = batch
+
+    B, lat, lon, time, var = dyn.shape
+
+    # For now (no time embeddings), just fold the time dimension into the var dimension.
+    dyn = dyn.reshape(B, lat, lon, time * var)
+
+    B, lat, lon, time, var = trg.shape
+    if time > 1:
+        raise NotImplementedError("Only single target time step is supported for now.")
+    trg = trg.reshape(B, lat, lon, time * var)
+
+    # Match convention of other datasets to have channels first.
+
+    # Convert to (B, time*var, lat, lon)
+    dyn = dyn.permute(0, 3, 1, 2)
+    trg = trg.permute(0, 3, 1, 2)
+
+    frac = torch.rand(1, generator=gen).item()
+    frac = frac * (max_frac - min_frac) + min_frac
+
+    # Generates an "image" of the same shape as x, with values between 0 and 1,
+    # and then compares it to frac to create a mask.
+    single_channel_x = dyn[:, 0:1, :, :]
+    mask = torch.empty_like(single_channel_x).uniform_(generator=gen) < frac
+    dyn_masked = dyn * mask
+
+    # Have dyn at the end, because it's used as the residual.
+
+    ctx = torch.cat([static, dyn_masked], dim=1)  # (B, static+dyn, lat, lon)
+
+    return ModelCtx(image_ctx=ctx), trg
